233efb1239b52a053831a84315140af7604b5afc
Shreya050302 <sharma.shreya5302@gmail.com>
2024-07-22 16:33:14 +0530

    voting test

---
+++ voting.java
@@ -0,0 +1,9 @@
+class StarPatten{
+    public static void main(String[] args) {
+        
+        int age =20;
+        String adult = (age >= 18) ? "eligible": "not eligible";
+        System.out.println(adult);
+        
+    }
+}
