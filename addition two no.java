62d9662e4b1c1b009745052c56c00cc87838b75d
Shreya050302 <sharma.shreya5302@gmail.com>
2024-07-22 15:38:01 +0530

    addition of two number

---
+++ addition two no.java
@@ -0,0 +1,8 @@
+class AddNumbers {
+    public static void main(String[] args) {
+        int a=5;
+        float b=7.5f;
+        float sum =a+b;
+        System.out.println("sum of two number:"+(a+b));
+    }
+}
